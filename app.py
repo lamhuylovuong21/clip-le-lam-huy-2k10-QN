026bcbfa4b833a8f89c14735ee14961f08d7089b
lamhuylovuong21 <lamhuylovuong2101@gmail.com>
2025-07-27 22:32:50 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 27 21:31:43 2025
+
+@author: ASUS
+"""
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads'
+
+@app.route("/")
+def home():
+    # Tạo thư mục nếu chưa có
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+
+    # Lấy danh sách file trong thư mục
+    files = os.listdir(UPLOAD_FOLDER)
+
+    return render_template("index.html", files=files)
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False)
+
+
+
+
+
