cb8d9f68ae937d6c1015dc8632b90744a89edd55
lamhuylovuong21 <lamhuylovuong2101@gmail.com>
2025-08-01 17:04:18 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ def home():
 
     return render_template("index.html", files=files)
 app.run(debug=True, use_reloader=False)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
 
 
 
